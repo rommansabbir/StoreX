ddcb15846f56ebe248dbe7b8c87dbec156e98719
Romman Sabbir <rommansabbir@gmail.com>
2022-08-20 18:31:45 +0600

    deprecation: save callback marked as @Deprecated

--- StoreX/src/main/java/com/rommansabbir/storex/callbacks/SaveCallback.kt
+++ StoreX/src/main/java/com/rommansabbir/storex/callbacks/SaveCallback.kt
@@ -2,6 +2,7 @@ package com.rommansabbir.storex.callbacks
 
 import com.rommansabbir.storex.StoreAbleObject
 
+@Deprecated("Will be removed in future version.")
 interface SaveCallback<T : StoreAbleObject> {
     fun onDone(value : T, exception: Exception? = null)
 }
