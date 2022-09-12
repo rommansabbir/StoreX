7440735d14c92eb181dadc77c5b9363def081bb4
Romman Sabbir <rommansabbir@gmail.com>
2022-09-12 17:00:14 +0600

    extensions added for Gson

--- StoreX/src/main/java/com/rommansabbir/storex/Extension.kt
+++ StoreX/src/main/java/com/rommansabbir/storex/Extension.kt
@@ -1,8 +1,9 @@
 package com.rommansabbir.storex
-
-
+import com.google.gson.Gson
 fun storeXInstance(config: StoreXConfig): StoreX = StoreXCore.instance(config)
 
 internal fun Subscriber.getKey(): String {
     return "${this.key}_${this.subscriberID}"
 }
+
+val gson by lazy { Gson() }
