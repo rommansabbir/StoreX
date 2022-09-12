9fa748a07d77dd54c5e62d1990989c9e3cdbd0b2
Romman Sabbir <rommansabbir@gmail.com>
2022-09-12 17:00:58 +0600

    serializer extensions added to convert `StoreAbleObject` to `Json` and vice versa

---
+++ StoreX/src/main/java/com/rommansabbir/storex/v2/SerializerExtenions.kt
@@ -0,0 +1,27 @@
+package com.rommansabbir.storex.v2
+
+import com.rommansabbir.storex.StoreAbleObject
+import com.rommansabbir.storex.gson
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
+fun StoreAbleObject.toJson(): String = gson.toJson(this)
+
+fun <T : StoreAbleObject> String.toStoreAbleObject(clazz: Class<T>): StoreAbleObject =
+    gson.fromJson(this, clazz)
+
+inline fun String.isJSONValid(crossinline callback: (Boolean) -> Unit={}): Boolean {
+    try {
+        JSONObject(this)
+    } catch (ex: JSONException) {
+        try {
+            JSONArray(this)
+        } catch (ex1: JSONException) {
+            callback.invoke(false)
+            return false
+        }
+    }
+    callback.invoke(true)
+    return true
+}
