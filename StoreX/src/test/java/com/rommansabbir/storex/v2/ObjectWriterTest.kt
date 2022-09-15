644af80ea1b3143b20558651d1251a8321742b2a
Romman Sabbir <rommansabbir@gmail.com>
2022-09-15 20:19:42 +0600

    test: verified tests for `ObjectWriter` implantation

---
+++ StoreX/src/test/java/com/rommansabbir/storex/v2/ObjectWriterTest.kt
@@ -0,0 +1,49 @@
+package com.rommansabbir.storex.v2
+
+import com.rommansabbir.storex.StoreAbleObject
+import com.rommansabbir.storex.v2.objectwritter.ObjectWriter
+import com.rommansabbir.storex.v2.objectwritter.ObjectWriterImpl
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.lang.ref.WeakReference
+
+@RunWith(RobolectricTestRunner::class)
+class ObjectWriterTest : BaseTestClass() {
+    private lateinit var objectWriter: ObjectWriter
+    private lateinit var testConfig: StoreXSmartConfig<StoreAbleObject>
+
+    @Before
+    fun setup() {
+        objectWriter = ObjectWriterImpl()
+        testConfig = ConfigUtils.getTestCacheDirConfig(WeakReference(context))
+    }
+
+    @Test
+    fun `write a test object`() {
+        val result = objectWriter.writeObject(
+            testConfig.getCacheDir(),
+            testConfig.fileName,
+            testConfig.xObject.toJson()
+        )
+        assert(result)
+    }
+
+    @Test
+    fun `get written object`() {
+        `write a test object`()
+        val writtenObject =
+            objectWriter.getWrittenObject(testConfig.getCacheDir(), testConfig.fileName)
+        val storedObject = writtenObject.toStoreAbleObject(StoreAbleObject::class.java)
+        assert(storedObject.objectId == testConfig.xObject.objectId)
+    }
+
+    @Test
+    fun `delete written object`() {
+        `get written object`()
+        val result =
+            objectWriter.deleteWrittenObject(testConfig.getCacheDir(), testConfig.fileName)
+        assert(result)
+    }
+}
