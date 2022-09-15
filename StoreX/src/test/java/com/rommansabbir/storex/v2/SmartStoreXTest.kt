e10ced43eaeea1d79922c87ce9c96fd8b8368da1
Romman Sabbir <rommansabbir@gmail.com>
2022-09-15 22:25:16 +0600

    test: added test for `SmartStoreX`, note: external dir test not completed

---
+++ StoreX/src/test/java/com/rommansabbir/storex/v2/SmartStoreXTest.kt
@@ -0,0 +1,71 @@
+package com.rommansabbir.storex.v2
+
+import com.rommansabbir.storex.StoreAbleObject
+import com.rommansabbir.storex.v2.smartstorex.SmartStoreX
+import com.rommansabbir.storex.v2.smartstorex.SmartStoreXImpl
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.lang.ref.WeakReference
+
+@RunWith(RobolectricTestRunner::class)
+class SmartStoreXTest : BaseTestClass() {
+    private lateinit var smartStoreX: SmartStoreX
+    private lateinit var cacheDirConfig: StoreXSmartConfig<StoreAbleObject>
+    private lateinit var filesDirConfig: StoreXSmartConfig<StoreAbleObject>
+    private lateinit var externalDirConfig: StoreXSmartConfig<StoreAbleObject>
+
+    @Before
+    fun setup() {
+        smartStoreX = SmartStoreXImpl()
+        cacheDirConfig = ConfigUtils.getTestCacheDirConfig(WeakReference(context))
+        filesDirConfig = ConfigUtils.getTestFilesDirConfig(WeakReference(context))
+        externalDirConfig = ConfigUtils.getTestFilesExternalConfig(WeakReference(context))
+    }
+
+    @Test
+    fun `save a test object to internal cache dir, get stored object and delete the object finally`() {
+        val uid = cacheDirConfig.xObject.objectId
+        val result = smartStoreX.set(cacheDirConfig)
+        assert(result)
+        val returnedResult = smartStoreX.get(
+            cacheDirConfig,
+            StoreAbleObject::class.java
+        )
+        assert(returnedResult is StoreAbleObject)
+        assert(returnedResult?.objectId == uid)
+        val isDeleted = smartStoreX.delete(cacheDirConfig)
+        assert(isDeleted)
+    }
+
+    @Test
+    fun `save a test object to internal files dir, get stored object and delete the object finally`() {
+        val uid = filesDirConfig.xObject.objectId
+        val result = smartStoreX.set(filesDirConfig)
+        assert(result)
+        val returnedResult = smartStoreX.get(
+            filesDirConfig,
+            StoreAbleObject::class.java
+        )
+        assert(returnedResult is StoreAbleObject)
+        assert(returnedResult?.objectId == uid)
+        val isDeleted = smartStoreX.delete(filesDirConfig)
+        assert(isDeleted)
+    }
+
+/*    @Test
+    fun `save a test object to internal external dir, get stored object and delete the object finally`() {
+        val uid = externalDirConfig.xObject.objectId
+        val result = smartStoreX.set(externalDirConfig)
+        assert(result)
+        val returnedResult = smartStoreX.get(
+            externalDirConfig,
+            StoreAbleObject::class.java
+        )
+        assert(returnedResult is StoreAbleObject)
+        assert(returnedResult?.objectId == uid)
+        val isDeleted = smartStoreX.delete(externalDirConfig)
+        assert(isDeleted)
+    }*/
+}
