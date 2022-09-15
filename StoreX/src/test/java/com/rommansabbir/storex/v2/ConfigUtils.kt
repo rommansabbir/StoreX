4c431a93cbe8c390cec0fa985767ff47151f3adf
Romman Sabbir <rommansabbir@gmail.com>
2022-09-15 22:24:44 +0600

    test: `ConfigUtils` to provide config for test purpose

--- StoreX/src/test/java/com/rommansabbir/storex/v2/ConfigUtils.kt
+++ StoreX/src/test/java/com/rommansabbir/storex/v2/ConfigUtils.kt
@@ -7,4 +7,10 @@ import java.lang.ref.WeakReference
 object ConfigUtils {
     fun getTestCacheDirConfig(context: WeakReference<Context>) =
         StoreXSmartConfig(context, "testfile", StoreAbleObject(), StoreXCachingStrategy.CacheDir())
+
+    fun getTestFilesDirConfig(context: WeakReference<Context>) =
+        StoreXSmartConfig(context, "testfile", StoreAbleObject(), StoreXCachingStrategy.FilesDir())
+
+    fun getTestFilesExternalConfig(context: WeakReference<Context>) =
+        StoreXSmartConfig(context, "testfile", StoreAbleObject(), StoreXCachingStrategy.ExternalDir())
 }
