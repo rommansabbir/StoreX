15247fda8158b35718032e0b5671eace2b9bbb71
Romman Sabbir <rommansabbir@gmail.com>
2022-09-15 22:22:52 +0600

    feat: `SmartStoreX` and `SmartStoreXImpl` added

---
+++ StoreX/src/main/java/com/rommansabbir/storex/v2/smartstorex/SmartStoreX.kt
@@ -0,0 +1,16 @@
+package com.rommansabbir.storex.v2.smartstorex
+
+import com.rommansabbir.storex.StoreAbleObject
+import com.rommansabbir.storex.v2.StoreXSmartConfig
+
+interface SmartStoreX {
+    @Throws(Exception::class)
+    fun <T : StoreAbleObject> set(config: StoreXSmartConfig<T>): Boolean
+
+    @Throws(Exception::class)
+    fun <T : StoreAbleObject> get(config: StoreXSmartConfig<T>, clazzName: Class<T>): T?
+
+    @Throws(Exception::class)
+    fun <T : StoreAbleObject> delete(config: StoreXSmartConfig<T>): Boolean
+}
+
