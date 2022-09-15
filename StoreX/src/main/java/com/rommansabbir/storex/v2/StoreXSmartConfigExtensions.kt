952c00777b7c3b9198ea68bfd3a54d5934aa20a8
Romman Sabbir <rommansabbir@gmail.com>
2022-09-15 22:23:36 +0600

    feat: several config extension functions added to get path for cache/files/external dir

--- StoreX/src/main/java/com/rommansabbir/storex/v2/StoreXSmartConfigExtensions.kt
+++ StoreX/src/main/java/com/rommansabbir/storex/v2/StoreXSmartConfigExtensions.kt
@@ -1,11 +1,12 @@
 package com.rommansabbir.storex.v2
 
+import android.os.Environment
 import com.rommansabbir.storex.StoreAbleObject
 
-inline fun <T : StoreAbleObject> StoreXSmartConfig<T>.getCacheDir(crossinline onPath: (String) -> Unit = {}): String {
+
+internal inline fun <T : StoreAbleObject> StoreXSmartConfig<T>.getCacheDir(crossinline onPath: (String) -> Unit = {}): String {
     if (this.context.get() == null) {
-        val message = "Context can't be null"
-        throw Exception(message)
+        throwException("Context can't be null")
     }
     val path = ((storeXCachingStrategy as StoreXCachingStrategy.CacheDir).path?.let
     { return@let "${this.context.get()!!.applicationContext?.cacheDir}/${it}" }
@@ -13,4 +14,47 @@ inline fun <T : StoreAbleObject> StoreXSmartConfig<T>.getCacheDir(crossinline on
         ?: "")
     onPath.invoke(path)
     return path
+}
+
+inline fun <T : StoreAbleObject> StoreXSmartConfig<T>.getFilesDir(crossinline onPath: (String) -> Unit = {}): String {
+    if (this.context.get() == null) {
+        throwException("Context can't be null")
+    }
+    val path = ((storeXCachingStrategy as StoreXCachingStrategy.FilesDir).path?.let
+    { return@let "${this.context.get()!!.applicationContext?.filesDir}/${it}" }
+        ?: kotlin.run { this.context.get()!!.applicationContext?.filesDir }?.absolutePath
+        ?: "")
+    onPath.invoke(path)
+    return path
+}
+
+inline fun <T : StoreAbleObject> StoreXSmartConfig<T>.getExternalDir(crossinline onPath: (String) -> Unit = {}): String {
+    if (this.context.get() == null) {
+        throwException("Context can't be null")
+    }
+    if (!isExternalStorageAvailable()) {
+        throwException("'External Storage' is not available")
+    }
+    if (isExternalStorageReadOnly()) {
+        throwException("'External Storage' is in `Read Only` mode")
+    }
+    val path = ((storeXCachingStrategy as StoreXCachingStrategy.ExternalDir).path?.let
+    { return@let "${this.context.get()!!.applicationContext?.getExternalFilesDir("Android/cache")}/${it}" }
+        ?: "")
+    onPath.invoke(path)
+    return path
+}
+
+fun throwException(message: String) {
+    throw Exception(message)
+}
+
+fun isExternalStorageReadOnly(): Boolean {
+    val extStorageState = Environment.getExternalStorageState()
+    return Environment.MEDIA_MOUNTED_READ_ONLY == extStorageState
+}
+
+fun isExternalStorageAvailable(): Boolean {
+    val extStorageState = Environment.getExternalStorageState()
+    return Environment.MEDIA_MOUNTED == extStorageState
 }
